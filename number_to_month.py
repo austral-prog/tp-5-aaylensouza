c69860c72a5c80c76a3547147912009716054e7b
aaylensouza <163536720+aaylensouza@users.noreply.github.com>
2024-06-07 11:31:59 -0300

    entrega number_to_month.py

--- number_to_month.py
+++ number_to_month.py
@@ -1,27 +1,28 @@
 def number_to_month(month):
-    if month ==1:
-        return "enero"
-    elif month ==2:
-        return "febrero"
-    elif month ==3:
-        return "marzo"
-    elif month ==4:
-        return "abril"
-    elif month ==5:
-        return "mayo"
-    elif month ==6:
-        return "junio"
-    elif month ==7:
-        return "julio"
-    elif month ==8:
-        return "agosto"
-    elif month ==9:
-        return "septiembre"
-    elif month ==10:
-        return "octubre"
-    elif month ==11:
-        return "noviembre"
-    elif month ==12:
-        return "diciembre"
-    else:
+    if month>=1 and 12>=month:
+        if month==1:
+            return "enero"
+        elif month==2:
+            return "febrero"
+        elif month==3:
+            return "marzo"
+        elif month==4:
+            return "abril"
+        elif month==5:
+            return "mayo"
+        elif month==6:
+            return "junio"
+        elif month==7:
+            return "julio"
+        elif month==8:
+            return "agosto"
+        elif month==9:
+            return "septiembre"
+        elif month==10:
+            return "octubre"
+        elif month==11:
+            return "noviembre"
+        elif month==12:
+            return "diciembre"
+    else: 
         return "error"
