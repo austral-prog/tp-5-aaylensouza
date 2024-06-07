95d8fbd1baa3749d80fe9e841614790f25ecf289
aaylensouza <163536720+aaylensouza@users.noreply.github.com>
2024-06-07 11:33:08 -0300

    entrega maximums.py

--- maximums.py
+++ maximums.py
@@ -1,16 +1,14 @@
 def max_of_two(x, y):
-    """Given x and y, that are 2 numbers, return the biggest number."""
-    if x > y:
-        return x
-    elif x < y:
-        return y
-    else: 
-        return x
-def max_of_three(x, y, z):
-    """Given x, y and z, that are 3 numbers, return the biggest number of the three."""
-    if x > y and x > z:
+    if x>y: 
         return x 
-    elif z > x and z > y:
-        return z
     else:
-        return y
+        return y 
+
+def max_of_three(x, y, z):
+    if x>y and x>z:
+        return x   
+    elif x<y and x>z:
+        return y 
+    else:
+        return z
+        
