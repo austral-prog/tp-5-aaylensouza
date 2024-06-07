bb80163972a22b263cb654259b92643158ccd424
aaylensouza <163536720+aaylensouza@users.noreply.github.com>
2024-06-07 11:27:26 -0300

    entrega quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,17 +1,18 @@
+# Replace the "ANSWER HERE" for your answer
+
 def roots(a, b, c):
-   dis = (b**2) - 4*a*c
-   if dis > 0:
-        r1 = (-b + dis**0.5) / (2*a)
-        r2 = (-b - dis**0.5) / (2*a)
-        return f"({r1}, {r2})"
-    elif dis == 0:
-        r1 = -b / (2 * a)
-        return f"({r1})"
-    else:
-        return "()"
+    discr = (b**2)-4*a*c
+    R1 = (-b + discr**0.5) / (2*a)
+    R2 = (-b - discr**0.5) / (2*a)
+    if  discr > 0:
+        return f"({R1}, {R2})"
+    if discr == 0:
+        R1 = -b / (2 * a)
+        return f"({R1})"
+    if discr < 0:
+        return "( )"
 
 def value_y(a, b, c, x):
-
     return (a*(x**2))+(b*x)+(c)
 
 def to_string(a, b, c):
@@ -32,17 +33,12 @@ def to_string(a, b, c):
     else:
         return "f(x) = 0"
 
-
 def derivation(a, b, c):
-
     if a != 0 and b != 0:
         return f"f'(x) = {2 * a} * X + {b}"
-
     elif a == 0 and b != 0:
         return f"f'(x) = {b}"
-
     elif a != 0 and b == 0:
         return f"f'(x) = {2 * a} * X"
-        
     else:
         return "f'(x) = 0"
